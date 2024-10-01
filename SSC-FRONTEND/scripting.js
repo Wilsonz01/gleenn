2865728db7671d6724a40fc83e95b76d5972e49f
survivor155 <wilson.navarez@lsu.edu.ph>
2024-10-01 13:47:17 +0800

    first commit

---
+++ SSC-FRONTEND/scripting.js
@@ -0,0 +1,13 @@
+<script>
+    {
+    document.getElementById('admin-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+        console.log('Email:', email);
+        console.log('Password:', password);
+
+        
+        // sumpayi loy para sa imo backend
+    });
+</script>
